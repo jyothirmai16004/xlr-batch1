5dd12bc93aaae38154ec3f60c0eefd7c2e8cdf64
jyothirmai16004 <jyothirmaijyothirmai1@gmail.com>
2025-01-24 11:25:50 +0530

    Add files via upload

---
+++ Demo4.java
@@ -0,0 +1,10 @@
+public class Demo4 {
+    public static void main(String[] args) {
+        String name = "jyothi";
+        String pwd = "123456";
+        while (("jyothi".equals(name) && "123456".equals(pwd))) {
+            System.out.println("hello welcome to mallareddy university");
+            break;
+        }
+    }
+}
