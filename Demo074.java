dc22708543394ce2062f11f89adb993563d5f0e8
jyothirmai16004 <jyothirmaijyothirmai1@gmail.com>
2025-02-10 11:19:48 +0530

    ort an array without predefined methods

---
+++ Demo074.java
@@ -0,0 +1,34 @@
+public class Demo074 {
+    public static void main(String[] args) {
+        int[] array = {5, 2, 8, 1, 9};
+
+        System.out.println("Original array: ");
+        printArray(array);
+
+        bubbleSort(array);
+
+        System.out.println("Sorted array: ");
+        printArray(array);
+    }
+
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Swap array[j] and array[j + 1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
